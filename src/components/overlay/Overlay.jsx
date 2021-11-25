119f91c1126a5e75a249f278164f7a86e61723b5
peripheralvisions <85910344+peripheralvisions@users.noreply.github.com>
2021-11-25 06:26:15 +0100

    Preventing re-run of the animation when calling function multiple times

--- src/components/overlay/Overlay.jsx
+++ src/components/overlay/Overlay.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useRef } from 'react';
 
 import { gsap } from 'gsap';
 
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-
 let pathRef = null;
+let isAnimating = false;
 
 export const overlayOpen = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+    
     const target = pathRef.current;
     gsap.timeline({
         onComplete: () => console.log('Complete')
@@ -23,11 +24,17 @@ export const overlayOpen = () => {
         .to(target, {
             duration: 0.3,
             ease: 'power2',
-            attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" }
+            attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" },
+            onComplete: () => {
+                isAnimating = false;
+            }
         })
 }
 
 export const overlayClose = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+
     const target = pathRef.current;
     gsap.timeline()
         .set(target, {
@@ -41,7 +48,10 @@ export const overlayClose = () => {
         .to(target, {
             duration: 0.8,
             ease: 'power4',
-            attr: { d: 'M 0 0 V 0 Q 50 0 100 0 V 0 z' }
+            attr: { d: 'M 0 0 V 0 Q 50 0 100 0 V 0 z' },
+            onComplete: () => {
+                isAnimating = false;
+            }
         })
 }
 
@@ -56,11 +66,11 @@ function Overlay() {
             preserveAspectRatio="none"
             width="100%"
             height="100%"
-            css={css`
-                position: absolute;
-                top: 0;
-                left: 0;
-            `}>
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+            }}>
             <path
                 ref={pathRef}
                 className="overlay__path"
